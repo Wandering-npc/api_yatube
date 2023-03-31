747f5e0f9e91870b4b7fd0e0bc463041ae7e68e8
WorkPC <you@example.com>
2023-03-31 15:04:31 +0300

    done

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,9 +8,8 @@ router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'groups', GroupViewSet, basename='groups')
 router.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, 
-                basename='comments',
-)
+                CommentViewSet,
+                basename='comments')
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
